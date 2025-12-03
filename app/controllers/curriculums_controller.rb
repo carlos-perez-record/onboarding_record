7bbb76d861cf3eee76c1f832e5906de078ca53f6
Carlos Pérez <carlos.perez@record.com.co>
2025-12-03 11:38:16 -0500

    feat: add custom language input field for curriculum

--- app/controllers/curriculums_controller.rb
+++ app/controllers/curriculums_controller.rb
@@ -9,6 +9,7 @@ class CurriculumsController < ApplicationController
 
   def create
     @curriculum = current_user.build_curriculum(curriculum_params)
+    merge_other_languages(@curriculum)
     if @curriculum.save
       redirect_to curriculum_path(@curriculum), notice: "Currículum registrado correctamente."
     else
@@ -24,6 +25,8 @@ class CurriculumsController < ApplicationController
 
   def update
     if @curriculum.update(curriculum_params)
+      merge_other_languages(@curriculum)
+      @curriculum.save
       redirect_to curriculum_path(@curriculum), notice: "Currículum actualizado correctamente."
     else
       render :edit, status: :unprocessable_entity
@@ -55,7 +58,15 @@ class CurriculumsController < ApplicationController
       :first_name, :last_name, :birth_date, :identification,
       :phone_number, :address, :city, :department, :country,
       :profile_description, :available_to_travel, :available_to_relocate,
-      :photo, languages: []
+      :photo, :other_languages, languages: []
     )
+  end
+
+  def merge_other_languages(curriculum)
+    other_langs = params[:curriculum][:other_languages].to_s.split(',').map(&:strip).reject(&:blank?)
+    if other_langs.any?
+      curriculum.languages = (curriculum.languages || []) + other_langs
+      curriculum.languages = curriculum.languages.uniq
+    end
   end
 end
