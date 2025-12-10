41bb66d30755ad0de7dfd9cc84a75e71eaefd7c1
Carlos Pérez <carlos.perez@record.com.co>
2025-12-10 14:55:35 -0500

    Fix: Agregar pines explícitos para controladores Stimulus

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,11 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
+# Pin individual controllers
+pin "controllers/confirm_controller", to: "confirm_controller.js"
+pin "controllers/feedback_controller", to: "feedback_controller.js"
+pin "controllers/form_loading_controller", to: "form_loading_controller.js"
+pin "controllers/modal_controller", to: "modal_controller.js"
+pin "controllers/toast_controller", to: "toast_controller.js"
+pin "controllers/validation_controller", to: "validation_controller.js"
+
